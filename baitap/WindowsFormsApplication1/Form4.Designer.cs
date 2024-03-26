18032a6f513e4696c27c7e523dd891377381b992
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:36:59 +0700

    all

---
+++ baitap/WindowsFormsApplication1/Form4.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.txtLop = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.MylistView = new System.Windows.Forms.ListView();
+            this.a = new System.Windows.Forms.Label();
+            this.txtGVCN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(308, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thêm lớp học";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên lớp:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số lượng sinh viên:";
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(336, 242);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(100, 20);
+            this.txtSoLuong.TabIndex = 3;
+            // 
+            // txtLop
+            // 
+            this.txtLop.Location = new System.Drawing.Point(336, 166);
+            this.txtLop.Name = "txtLop";
+            this.txtLop.Size = new System.Drawing.Size(100, 20);
+            this.txtLop.TabIndex = 4;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnThem.Location = new System.Drawing.Point(540, 169);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(135, 76);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // MylistView
+            // 
+            this.MylistView.GridLines = true;
+            this.MylistView.Location = new System.Drawing.Point(94, 345);
+            this.MylistView.Name = "MylistView";
+            this.MylistView.Size = new System.Drawing.Size(500, 97);
+            this.MylistView.TabIndex = 7;
+            this.MylistView.UseCompatibleStateImageBehavior = false;
+            this.MylistView.View = System.Windows.Forms.View.Details;
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Location = new System.Drawing.Point(102, 310);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(37, 13);
+            this.a.TabIndex = 8;
+            this.a.Text = "GVCN";
+            // 
+            // txtGVCN
+            // 
+            this.txtGVCN.Location = new System.Drawing.Point(336, 303);
+            this.txtGVCN.Name = "txtGVCN";
+            this.txtGVCN.Size = new System.Drawing.Size(100, 20);
+            this.txtGVCN.TabIndex = 9;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 522);
+            this.Controls.Add(this.txtGVCN);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.MylistView);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtLop);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.TextBox txtLop;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.ListView MylistView;
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.TextBox txtGVCN;
+    }
+}
---
+++ baitap/WindowsFormsApplication1/Form5.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TV_Test = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTieuDe = new System.Windows.Forms.TextBox();
+            this.btnthemgoc = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnXoaTatCa = new System.Windows.Forms.Button();
+            this.btnXoaChon = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnMoRong = new System.Windows.Forms.Button();
+            this.btnThuNho = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TV_Test
+            // 
+            this.TV_Test.LabelEdit = true;
+            this.TV_Test.Location = new System.Drawing.Point(23, 84);
+            this.TV_Test.Name = "TV_Test";
+            this.TV_Test.Size = new System.Drawing.Size(248, 316);
+            this.TV_Test.TabIndex = 0;
+            this.TV_Test.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TV_Test_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TreeView";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tiêu đều node:";
+            // 
+            // txtTieuDe
+            // 
+            this.txtTieuDe.Location = new System.Drawing.Point(458, 53);
+            this.txtTieuDe.Name = "txtTieuDe";
+            this.txtTieuDe.Size = new System.Drawing.Size(100, 20);
+            this.txtTieuDe.TabIndex = 3;
+            // 
+            // btnthemgoc
+            // 
+            this.btnthemgoc.Location = new System.Drawing.Point(469, 103);
+            this.btnthemgoc.Name = "btnthemgoc";
+            this.btnthemgoc.Size = new System.Drawing.Size(75, 23);
+            this.btnthemgoc.TabIndex = 4;
+            this.btnthemgoc.Text = "Thêm node gốc";
+            this.btnthemgoc.UseVisualStyleBackColor = true;
+            this.btnthemgoc.Click += new System.EventHandler(this.btnthemgoc_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(436, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Thêm node con tại vị trí";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnXoaTatCa
+            // 
+            this.btnXoaTatCa.Location = new System.Drawing.Point(469, 235);
+            this.btnXoaTatCa.Name = "btnXoaTatCa";
+            this.btnXoaTatCa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoaTatCa.TabIndex = 6;
+            this.btnXoaTatCa.Text = "Xóa tất cả các node";
+            this.btnXoaTatCa.UseVisualStyleBackColor = true;
+            this.btnXoaTatCa.Click += new System.EventHandler(this.btnXoaTatCa_Click);
+            // 
+            // btnXoaChon
+            // 
+            this.btnXoaChon.Location = new System.Drawing.Point(469, 286);
+            this.btnXoaChon.Name = "btnXoaChon";
+            this.btnXoaChon.Size = new System.Drawing.Size(75, 23);
+            this.btnXoaChon.TabIndex = 7;
+            this.btnXoaChon.Text = "Xóa node đang chọn";
+            this.btnXoaChon.UseVisualStyleBackColor = true;
+            this.btnXoaChon.Click += new System.EventHandler(this.btnXoaChon_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(469, 337);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnMoRong
+            // 
+            this.btnMoRong.Location = new System.Drawing.Point(36, 454);
+            this.btnMoRong.Name = "btnMoRong";
+            this.btnMoRong.Size = new System.Drawing.Size(75, 23);
+            this.btnMoRong.TabIndex = 9;
+            this.btnMoRong.Text = "+";
+            this.btnMoRong.UseVisualStyleBackColor = true;
+            this.btnMoRong.Click += new System.EventHandler(this.btnMoRong_Click);
+            // 
+            // btnThuNho
+            // 
+            this.btnThuNho.Location = new System.Drawing.Point(151, 454);
+            this.btnThuNho.Name = "btnThuNho";
+            this.btnThuNho.Size = new System.Drawing.Size(75, 23);
+            this.btnThuNho.TabIndex = 10;
+            this.btnThuNho.Text = "-";
+            this.btnThuNho.UseVisualStyleBackColor = true;
+            this.btnThuNho.Click += new System.EventHandler(this.btnThuNho_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 525);
+            this.Controls.Add(this.btnThuNho);
+            this.Controls.Add(this.btnMoRong);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoaChon);
+            this.Controls.Add(this.btnXoaTatCa);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnthemgoc);
+            this.Controls.Add(this.txtTieuDe);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TV_Test);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TV_Test;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTieuDe;
+        private System.Windows.Forms.Button btnthemgoc;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnXoaTatCa;
+        private System.Windows.Forms.Button btnXoaChon;
+        private System.Windows.Forms.Button btnMoRong;
+        private System.Windows.Forms.Button btnThuNho;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
