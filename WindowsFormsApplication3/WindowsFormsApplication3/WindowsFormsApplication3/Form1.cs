4a26673156adf24af77ed2275508d34633ffd4f7
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:42:43 +0700

    form chính

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fr2 = new Form2();
+            fr2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form3 fr3 = new form3();
+            fr3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 fr4 = new Form4();
+            fr4.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            form5 fr5 = new form5();
+            fr5.Show();
+        }
+    }
+}
+ 
