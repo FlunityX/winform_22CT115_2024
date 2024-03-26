3254a03139eef831f9418b3c1d3b867324623ec1
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:35:33 +0700

    ListView

---
+++ baitap/WindowsFormsApplication1/Form4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (txtSoLuong.Text != "" && txtLop.Text != "")
+            {
+                ListViewItem LVItem = new ListViewItem(txtLop.Text);
+                ListViewItem.ListViewSubItem LVSItem = new
+                ListViewItem.ListViewSubItem(LVItem, txtSoLuong.Text);
+                LVItem.SubItems.Add(LVSItem);
+                LVSItem = new ListViewItem.ListViewSubItem(LVItem,
+                txtGVCN.Text);
+                LVItem.SubItems.Add(LVSItem);
+                MylistView.Items.Add(LVItem);
+            }
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            MylistView.Columns.Add("Tên Lớp", 160);
+            MylistView.Columns.Add("Số lượng sinh viên", 140);
+            MylistView.Columns.Add("GVCN", 100);
+        }
+    }
+}
