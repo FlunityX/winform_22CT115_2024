9dcad6a89b011d56caffb611027cd90dd5521889
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 14:45:01 +0700

    DateTimePicker

---
+++ bài_tập/bài_tập/Form3.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace bài_tập
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyMCalendar = new System.Windows.Forms.MonthCalendar();
+            this.btnHienThi = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MyMCalendar
+            // 
+            this.MyMCalendar.Location = new System.Drawing.Point(206, 123);
+            this.MyMCalendar.Name = "MyMCalendar";
+            this.MyMCalendar.TabIndex = 0;
+            // 
+            // btnHienThi
+            // 
+            this.btnHienThi.Location = new System.Drawing.Point(139, 360);
+            this.btnHienThi.Name = "btnHienThi";
+            this.btnHienThi.Size = new System.Drawing.Size(75, 23);
+            this.btnHienThi.TabIndex = 1;
+            this.btnHienThi.Text = "hiển thị ";
+            this.btnHienThi.UseVisualStyleBackColor = true;
+            this.btnHienThi.Click += new System.EventHandler(this.btnHienThi_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(432, 360);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 451);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHienThi);
+            this.Controls.Add(this.MyMCalendar);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar MyMCalendar;
+        private System.Windows.Forms.Button btnHienThi;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
