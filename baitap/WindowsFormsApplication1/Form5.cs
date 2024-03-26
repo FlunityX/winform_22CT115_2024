2cb395dac7cacdcdd72bccae0b5f5bebae7f6bd5
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:36:20 +0700

    Tree View

---
+++ baitap/WindowsFormsApplication1/Form5.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TV_Test.SelectedNode.Nodes.Add(txtTieuDe.Text);
+           txtTieuDe.Text = ""; TV_Test.ExpandAll();
+        }
+
+        private void btnthemgoc_Click(object sender, EventArgs e)
+        {
+            TV_Test.Nodes.Add(txtTieuDe.Text);
+            txtTieuDe.Text = "";
+        }
+
+        private void btnXoaTatCa_Click(object sender, EventArgs e)
+        {
+            TV_Test.Nodes.Clear();
+        }
+
+        private void btnMoRong_Click(object sender, EventArgs e)
+        {
+            TV_Test.ExpandAll();
+        }
+
+        private void btnThuNho_Click(object sender, EventArgs e)
+        {
+            TV_Test.CollapseAll();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnXoaChon_Click(object sender, EventArgs e)
+        {
+            TV_Test.SelectedNode.Remove();
+        }
+
+        private void TV_Test_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+      
+        
+    }
+}
