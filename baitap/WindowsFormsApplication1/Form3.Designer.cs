6a902a58deb2209869ac52bdd28d8fba590f8117
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:34:30 +0700

    tooltip

---
+++ baitap/WindowsFormsApplication1/Form3.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.ProGressTG = new System.Windows.Forms.ProgressBar();
+            this.ThoiGian = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phương trình bậc nhất: ax + b = 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hệ số a:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hệ số b:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nhập nghiệm:";
+            // 
+            // txtA
+            // 
+            this.txtA.Enabled = false;
+            this.txtA.Location = new System.Drawing.Point(259, 107);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 4;
+            // 
+            // txtB
+            // 
+            this.txtB.Enabled = false;
+            this.txtB.Location = new System.Drawing.Point(259, 170);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 5;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(259, 246);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 6;
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Location = new System.Drawing.Point(131, 314);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(75, 23);
+            this.btnKiemTra.TabIndex = 7;
+            this.btnKiemTra.Text = "kiểm tra";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(384, 314);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "thoát ";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // ProGressTG
+            // 
+            this.ProGressTG.Location = new System.Drawing.Point(157, 372);
+            this.ProGressTG.Maximum = 30000;
+            this.ProGressTG.Name = "ProGressTG";
+            this.ProGressTG.Size = new System.Drawing.Size(254, 23);
+            this.ProGressTG.Step = 1000;
+            this.ProGressTG.TabIndex = 9;
+            this.ProGressTG.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // ThoiGian
+            // 
+            this.ThoiGian.Enabled = true;
+            this.ThoiGian.Interval = 1000;
+            this.ThoiGian.Tick += new System.EventHandler(this.ThoiGian_Tick);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 431);
+            this.Controls.Add(this.ProGressTG);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ProgressBar ProGressTG;
+        private System.Windows.Forms.Timer ThoiGian;
+    }
+}
---
+++ baitap/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        private int a = 0; 
+        private int b = 0; 
+        private float x = 0;
+        Random rd = new Random();
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            a = rd.Next(-10, 10);
+            txtA.Text = a.ToString();
+            b = rd.Next(-10, 10);
+            txtB.Text = b.ToString();
+            x = -b / (float)a;
+        }
+
+        private void ThoiGian_Tick(object sender, EventArgs e)
+        {
+            if (ProGressTG.Value == 30000)
+            {
+                ThoiGian.Enabled = false;
+                MessageBox.Show("Hết giờ làm bài");
+            }
+            ProGressTG.PerformStep();
+        }
+
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            float kq = float.Parse(txtX.Text);
+            if (Math.Abs(kq - x) < 0.01)
+            {
+                MessageBox.Show("Bạn đã làm đúng");
+                Close();
+            }
+            else
+                MessageBox.Show("Bạn đã trả lời sai");
+        }
+    }
+}
