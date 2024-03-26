c6976d33f66236f3f1596744008eab8ae5fc0bc2
nguyễn mạnh <manharia123@gmail.com>
2024-03-26 15:14:11 +0700

    Notifly Icon

---
+++ bai_tap2/WindowsFormsApplication1/WindowsFormsApplication1/Form3.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.MyContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MyNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MyContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Khoa Công nghệ thông tin";
+            // 
+            // MyContextMenuStrip
+            // 
+            this.MyContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thoátToolStripMenuItem});
+            this.MyContextMenuStrip.Name = "MyContextMenuStrip";
+            this.MyContextMenuStrip.Size = new System.Drawing.Size(241, 28);
+            // 
+            // MyNotifyIcon
+            // 
+            this.MyNotifyIcon.ContextMenuStrip = this.MyContextMenuStrip;
+            this.MyNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("MyNotifyIcon.Icon")));
+            this.MyNotifyIcon.Text = "notifyIcon1";
+            this.MyNotifyIcon.Visible = true;
+            this.MyNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MyNotifyIcon_MouseClick);
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(240, 24);
+            this.thoátToolStripMenuItem.Text = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 466);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NotifyIcon";
+            this.MyContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip MyContextMenuStrip;
+        private System.Windows.Forms.NotifyIcon MyNotifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+    }
+}
